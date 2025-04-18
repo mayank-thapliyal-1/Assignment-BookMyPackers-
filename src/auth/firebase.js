39358fc2ccefb2710b1bb518cf234491ea79b826
moonMaster922 <mayankthapliyal100@gmail.com>
2025-04-18 16:34:17 +0530

    tempory disabeled  firebase file

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,19 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// import { getAnalytics } from "firebase/analytics"; // Optional
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // import { getAnalytics } from "firebase/analytics"; // Optional
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "apna-e154a.firebaseapp.com",
-  projectId: "apna-e154a",
-  storageBucket: "apna-e154a.appspot.com", 
-  messagingSenderId: "154059837539",
-  appId: "1:154059837539:web:26ca91f9caee19d8512856",
-  measurementId: "G-8PQ3Z87146"
-};
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app); // Initialize auth after the app
-export default auth;
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: "apna-e154a.firebaseapp.com",
+//   projectId: "apna-e154a",
+//   storageBucket: "apna-e154a.appspot.com", 
+//   messagingSenderId: "154059837539",
+//   appId: "1:154059837539:web:26ca91f9caee19d8512856",
+//   measurementId: "G-8PQ3Z87146"
+// };
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app); // Initialize auth after the app
+// export default auth;
 // Optional — only if needed
 // let analytics;
 // if (typeof window !== "undefined") {
